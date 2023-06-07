d3415ace654acb9c12d279192b2177e63b2b613a
FrancisJTR7 <xgalasko@outlook.com>
2023-06-07 18:14:31 -0400

    made skelet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import hotBg from './assets/hot.png';
+import coldBg from './assets/cold.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className='overlay'>
+        <div className='container'>
+          <div className='section section__inputs'>
+            <input type='text' name='city' placeholder='Enter City...' />
+            <button>°F</button>
+          </div>
+          <div className='section section__temperatur'>
+            <div className='icon'>
+              <h3>London, GB</h3>
+              <img
+                src='https://icons.veryicon.com/png/o/miscellaneous/test-6/weather-91.png'
+                alt='weatherIcon'
+              />
+              <h3>Cloudy</h3>
+            </div>
+            <div className='temperature'>
+              <h1>34 °C</h1>
+            </div>
+          </div>
+
+          {/*bottom description*/}
+        </div>
+      </div>
     </div>
   );
 }
